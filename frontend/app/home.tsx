3a59c772907bf244f37a461e2ecb6b3a1b611146
darkevo2424 <timotius.albert@luxpmsoft.com>
2023-01-19 13:40:24 +0700

    updated

--- frontend/app/home.tsx
+++ frontend/app/home.tsx
@@ -27,7 +27,7 @@ export default async function Component() {
   // }
   return (
     <div className='flex justify-center p-20 flex-col'>
-      <p >Hello ! your email is <span className=' underline'></span></p>
+      <p >Hello ! your email is <span className=' underline'>{session?.user?.email}</span></p>
       <button onClick={() => signOut()} type="submit" class="w-40 text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Logout</button>
     </div>
   )
